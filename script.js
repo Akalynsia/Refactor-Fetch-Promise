8ff669038d28c84cb97932da64618e92d99e5dc8
Oktay HIZAR <96901734+Akalynsia@users.noreply.github.com>
2023-11-07 01:13:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// API'dan çektiğim kullanıcı isimlerini butona tıkladığımda ekranda görmek istiyorum. Bunun için kodu tekrar gözden geçirmeniz gerekecek
+
+const button = document.querySelector('button');
+const ul = document.querySelector('ul');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((res) => res.json())
+    .then(data => {
+      const names = data.map(user => user.name);
+      names.forEach(name => {
+        const li = document.createElement('li');
+        li.textContent = name;
+        ul.appendChild(li);
+      });
+    })
+});
